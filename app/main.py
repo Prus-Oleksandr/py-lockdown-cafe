6e097639f51683fcef08306b22a89f39ccf4006f
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-04-14 12:35:04 +0300

    Fix type annotations and mask count condition in go_to_cafe

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from app.errors import (
 )
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
     unvaccinated_friend = False
     masks_to_buy = 0
     for friend in friends:
@@ -17,6 +17,6 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
             masks_to_buy += 1
     if unvaccinated_friend:
         return "All friends should be vaccinated"
-    if masks_to_buy > 0:
+    if masks_to_buy != 0:
         return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
